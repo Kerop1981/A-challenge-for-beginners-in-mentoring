d559225ad415b8515cf2837b8fa4b163dfb1d0c8
Kerop1981 <vova.abraamyan@mail.ru>
2023-12-27 07:50:11 +0300

    Commit Bro

--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -2,17 +2,22 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user-card',
-  standalone: true,
-  imports: [],
+  standalone:true,
+  imports:[],
   templateUrl: './user-card.component.html',
-  styleUrl: './user-card.component.css'
+  styleUrls: ['./user-card.component.css']
 })
 export class UserCardComponent {
-  user = {id:1,name:'vladimir abraamyan', email:'vova.abraamyan@mail.ru'}
-  user1 = {id:2,name:'Maik TYson'}
-@Output() deleteUserEvent = new EventEmitter<number>();
 
-deleteUser(){
-  this.deleteUserEvent.emit(this.user.id)
-}
+  @Input()
+  user!: { id: number; name: string; email: string; }; 
+  @Output() deleteUserEvent = new EventEmitter<number>();
+
+  constructor(){
+    this.user = { id: 1, name: '', email: '' }; 
+  }
+
+  deleteUser(): void {
+    this.deleteUserEvent.emit(this.user.id);
+  }
 }
